a094c8733053d07581de9b0cd1e0371a01f559c6
dpescada <darionpescada@gmail.com>
2022-11-28 13:04:32 -0800

    added summary and confirmation

---
+++ CarPoolingApp/app/src/main/java/com/example/carpoolingapp/bookingSummary.java
@@ -0,0 +1,14 @@
+package com.example.carpoolingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bookingSummary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking_summary);
+    }
+}
