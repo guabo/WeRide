9d118a90477f89bc8f71c46beb8853fbe62af0c0
dpescada <darionpescada@gmail.com>
2022-11-28 12:19:32 -0800

    driver rating

---
+++ CarPoolingApp/app/src/main/java/com/example/carpoolingapp/rateDriver.java
@@ -0,0 +1,14 @@
+package com.example.carpoolingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class rateDriver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate_driver);
+    }
+}
