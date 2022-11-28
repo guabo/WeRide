82c7e932acc8ff74daf95f7d1a2757f216574bb2
jesselazzari <jesselazzari@yahoo.ca>
2022-11-28 13:39:34 -0800

    added buttons

--- CarPoolingApp/app/src/main/java/com/example/carpoolingapp/AvailablePassengers.java
+++ CarPoolingApp/app/src/main/java/com/example/carpoolingapp/AvailablePassengers.java
@@ -2,13 +2,40 @@ package com.example.carpoolingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AvailablePassengers extends AppCompatActivity {
+    Intent intent1;
+    Intent intent2;
+    Button home;
+    Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_available_passengers);
+
+
+        home = findViewById(R.id.button10);
+        start = findViewById(R.id.button8);
+
+        home.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v) {
+                intent1 = new Intent(AvailablePassengers.this, Booking.class);
+                startActivity(intent1);
+            }
+        });
+        start.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v) {
+                intent2 = new Intent(AvailablePassengers.this, bookingConfirmation.class);
+                startActivity(intent2);
+            }
+        });
+
     }
 }
--- CarPoolingApp/app/src/main/java/com/example/carpoolingapp/CreateAccount.java
+++ CarPoolingApp/app/src/main/java/com/example/carpoolingapp/CreateAccount.java
@@ -2,13 +2,39 @@ package com.example.carpoolingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateAccount extends AppCompatActivity {
+    Intent intent1;
+    Intent intent2;
+    Button signup;
+    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        signup = findViewById(R.id.button9);
+        back = findViewById(R.id.button7);
+
+        signup.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v) {
+                intent1 = new Intent(CreateAccount.this, Booking.class);
+                startActivity(intent1);
+            }
+        });
+        back.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v) {
+                intent2 = new Intent(CreateAccount.this, MainActivity.class);
+                startActivity(intent2);
+            }
+        });
+
     }
 }
