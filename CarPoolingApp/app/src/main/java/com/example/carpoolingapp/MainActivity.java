dfc0fd1ed37c041897c68e0aad5ff33e7d675e92
jesselazzari <jesselazzari@yahoo.ca>
2022-11-28 13:00:20 -0800

    added first button

--- CarPoolingApp/app/src/main/java/com/example/carpoolingapp/MainActivity.java
+++ CarPoolingApp/app/src/main/java/com/example/carpoolingapp/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.carpoolingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Intent intent1;
+    Button loginButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = findViewById(R.id.button2);
+
+        loginButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v) {
+                intent1 = new Intent(MainActivity.this, Booking.class);
+                startActivity(intent1);
+            }
+        });
+
     }
+
+
 }
