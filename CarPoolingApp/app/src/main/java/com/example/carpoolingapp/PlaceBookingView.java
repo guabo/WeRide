e45fd9403b674c78237dc6c3ebce17fd62950f46
jesselazzari <jesselazzari@yahoo.ca>
2022-11-28 14:11:54 -0800

    added simulation button

--- CarPoolingApp/app/src/main/java/com/example/carpoolingapp/PlaceBookingView.java
+++ CarPoolingApp/app/src/main/java/com/example/carpoolingapp/PlaceBookingView.java
@@ -1,13 +1,41 @@
 package com.example.carpoolingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PlaceBookingView extends AppCompatActivity {
+
+    Intent intent1;
+    Intent intent2;
+    Button home;
+    Button bookNow;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_booking_view);
+
+        home = findViewById(R.id.button12);
+        bookNow = findViewById(R.id.button11);
+
+        home.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v) {
+                intent1 = new Intent(PlaceBookingView.this, Booking.class);
+                startActivity(intent1);
+            }
+        });
+        bookNow.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v) {
+                intent2 = new Intent(PlaceBookingView.this, YourBookings.class);
+                startActivity(intent2);
+            }
+        });
     }
 }
