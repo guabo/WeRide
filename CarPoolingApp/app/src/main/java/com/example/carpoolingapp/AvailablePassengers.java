7e4287e0914973ac74c2507ea7f72f046b1da335
guabo <gmgabrielmercier@gmail.com>
2022-11-30 20:57:19 -0800

    feedback implementation

--- CarPoolingApp/app/src/main/java/com/example/carpoolingapp/AvailablePassengers.java
+++ CarPoolingApp/app/src/main/java/com/example/carpoolingapp/AvailablePassengers.java
@@ -6,12 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class AvailablePassengers extends AppCompatActivity {
     Intent intent1;
     Intent intent2;
     Button home;
     Button start;
+    CheckBox c1;
+    CheckBox c2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,8 @@ public class AvailablePassengers extends AppCompatActivity {
 
         home = findViewById(R.id.button10);
         start = findViewById(R.id.button8);
+        c1 = findViewById(R.id.checkBox);
+        c2 = findViewById(R.id.checkBox2);
 
         home.setOnClickListener(new View.OnClickListener()
         {
@@ -32,10 +38,20 @@ public class AvailablePassengers extends AppCompatActivity {
         start.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View v) {
-                intent2 = new Intent(AvailablePassengers.this, bookingSummary.class);
-                startActivity(intent2);
+
+                if (!c1.isChecked() && !c2.isChecked()){
+                    toast();
+                }else{
+                    intent2 = new Intent(AvailablePassengers.this, bookingSummary.class);
+                    startActivity(intent2);
+                }
+
             }
         });
 
+    }
+    public void toast()
+    {
+        Toast.makeText(this, "Please check a box to accept a job", Toast.LENGTH_SHORT).show();
     }
 }
